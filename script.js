878a22aa72e92027345a513784653528100589d3
ezycardiansyah <74405709+ezycardiansyah@users.noreply.github.com>
2026-04-17 16:40:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,27 +12,27 @@ const materiData = {
         "https://docs.google.com/presentation/d/1HPhwFV5DUsit1mmEZpHQYWNOFEonnEtM/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
         "https://docs.google.com/presentation/d/12PFVGcjT2bWrg4_jCyO7RRoPnQLFX9yR/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
         "https://docs.google.com/presentation/d/164DGgQDrdR_XR7g72fTUpMupdH54EZmZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1SOnK3RVjZqOnWkWkmi2fRp9_TuQZoXmL/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true"
-        "https://docs.google.com/presentation/d/1euF9dFL3_E0fa4i5SAmwI8G4jpIZj_br/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true"
+        "https://docs.google.com/presentation/d/1SOnK3RVjZqOnWkWkmi2fRp9_TuQZoXmL/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
+        "https://docs.google.com/presentation/d/1euF9dFL3_E0fa4i5SAmwI8G4jpIZj_br/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
     ],
 
     arkom: [
         "https://docs.google.com/presentation/d/1OwjOPa1JU44WSVBULojch8pyQHuHbu-v/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
         "https://docs.google.com/presentation/d/186gFqXgFqN9v2HG2JK2tVR8Jj5bAhhNt/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
         "https://docs.google.com/presentation/d/1xA34MmpcZ7MEp3Qk1MvXQGeNc_Ar6-Hr/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/18_4SRBptVTsDmdj_8WYDiLtg8LE_7dDZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true"
+        "https://docs.google.com/presentation/d/18_4SRBptVTsDmdj_8WYDiLtg8LE_7dDZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
     ],
 
     statistika: [
         "https://docs.google.com/presentation/d/123FdM-YHtdiPKRoxJic2znIm-llG1NGZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
         "https://docs.google.com/presentation/d/1k6EDWHwrzxrmlbERgb69dtaODNsM3p85/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1cnXAETIIXfx11wo0w1xnvjNLG-c9egYT/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true"
+        "https://docs.google.com/presentation/d/1cnXAETIIXfx11wo0w1xnvjNLG-c9egYT/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
     ],
 
     algoritma: [
         "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1-P6wl73IczSQdgesgQqtphGDNRuP8e1N&embedded=true",
         "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1Z2iS_kewGKisuumy69XlA08OuOnLwDXI&embedded=true",
-        "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1vlGC064RplAIsDcgqkVQQta_nNmATniE&embedded=true"
+        "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1vlGC064RplAIsDcgqkVQQta_nNmATniE&embedded=true",
     ]
 
 };
