01217f648e9ad00d778de61da53b251499488c94
ezycardiansyah <74405709+ezycardiansyah@users.noreply.github.com>
2026-04-17 16:41:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,36 +9,49 @@ const courses = {
 const materiData = {
 
     instrumentasi: [
-        "https://docs.google.com/presentation/d/1HPhwFV5DUsit1mmEZpHQYWNOFEonnEtM/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/12PFVGcjT2bWrg4_jCyO7RRoPnQLFX9yR/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/164DGgQDrdR_XR7g72fTUpMupdH54EZmZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1SOnK3RVjZqOnWkWkmi2fRp9_TuQZoXmL/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1euF9dFL3_E0fa4i5SAmwI8G4jpIZj_br/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
+        convertSlide("1HPhwFV5DUsit1mmEZpHQYWNOFEonnEtM"),
+        convertSlide("12PFVGcjT2bWrg4_jCyO7RRoPnQLFX9yR"),
+        convertSlide("164DGgQDrdR_XR7g72fTUpMupdH54EZmZ"),
+        convertSlide("1SOnK3RVjZqOnWkWkmi2fRp9_TuQZoXmL"),
+        convertSlide("1euF9dFL3_E0fa4i5SAmwI8G4jpIZj_br")
     ],
 
     arkom: [
-        "https://docs.google.com/presentation/d/1OwjOPa1JU44WSVBULojch8pyQHuHbu-v/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/186gFqXgFqN9v2HG2JK2tVR8Jj5bAhhNt/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1xA34MmpcZ7MEp3Qk1MvXQGeNc_Ar6-Hr/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/18_4SRBptVTsDmdj_8WYDiLtg8LE_7dDZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
+        convertSlide("1OwjOPa1JU44WSVBULojch8pyQHuHbu-v"),
+        convertSlide("186gFqXgFqN9v2HG2JK2tVR8Jj5bAhhNt"),
+        convertSlide("1xA34MmpcZ7MEp3Qk1MvXQGeNc_Ar6-Hr"),
+        convertSlide("18_4SRBptVTsDmdj_8WYDiLtg8LE_7dDZ")
     ],
 
     statistika: [
-        "https://docs.google.com/presentation/d/123FdM-YHtdiPKRoxJic2znIm-llG1NGZ/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1k6EDWHwrzxrmlbERgb69dtaODNsM3p85/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
-        "https://docs.google.com/presentation/d/1cnXAETIIXfx11wo0w1xnvjNLG-c9egYT/edit?usp=drive_link&ouid=114736929292569281585&rtpof=true&sd=true",
+        convertSlide("123FdM-YHtdiPKRoxJic2znIm-llG1NGZ"),
+        convertSlide("1k6EDWHwrzxrmlbERgb69dtaODNsM3p85"),
+        convertSlide("1cnXAETIIXfx11wo0w1xnvjNLG-c9egYT")
     ],
 
     algoritma: [
-        "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1-P6wl73IczSQdgesgQqtphGDNRuP8e1N&embedded=true",
-        "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1Z2iS_kewGKisuumy69XlA08OuOnLwDXI&embedded=true",
-        "https://docs.google.com/gview?url=https://drive.google.com/uc?id=1vlGC064RplAIsDcgqkVQQta_nNmATniE&embedded=true",
+        convertDrive("1-P6wl73IczSQdgesgQqtphGDNRuP8e1N"),
+        convertDrive("1Z2iS_kewGKisuumy69XlA08OuOnLwDXI"),
+        convertDrive("1vlGC064RplAIsDcgqkVQQta_nNmATniE")
     ]
-
 };
 
 
-// buka praktikum
+// ===== FUNCTION KONVERSI LINK =====
+
+// untuk Google Slides
+function convertSlide(id) {
+    return `https://docs.google.com/gview?url=https://docs.google.com/presentation/d/${id}/edit&embedded=true`;
+}
+
+// untuk Google Drive file
+function convertDrive(id) {
+    return `https://docs.google.com/gview?url=https://drive.google.com/uc?id=${id}&embedded=true`;
+}
+
+
+// ===== FUNCTION UTAMA =====
+
 function openCourse(key) {
     document.getElementById("mainPage").style.display = "none";
     document.getElementById("coursePage").style.display = "block";
@@ -69,21 +82,18 @@ function openCourse(key) {
 }
 
 
-// kembali
 function goBack() {
     document.getElementById("mainPage").style.display = "grid";
     document.getElementById("coursePage").style.display = "none";
 }
 
 
-// preview
 function previewPPT(url) {
     document.getElementById("modal").style.display = "block";
     document.getElementById("viewer").src = url;
 }
 
 
-// close
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
